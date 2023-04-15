cde2927125978f051bce5f3e41a1e89711f781cb
Naman <namansngh64@gmail.com>
2023-04-15 11:47:57 +0530

    render hax

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+const port = 8500;
+
+//Middlewares
+app.use(
+  cors({
+    credentials: true,
+    origin: "*"
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Starting Server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Test..." });
+});
+const keepAlive2 = async () => {
+  console.log("running keepAlive");
+  const ping = await axios
+    .get("https://reder-hax.onrender.com/")
+    .catch((error) => {
+      console.log(error);
+    });
+  console.log(ping.data);
+};
+
+const keepAlive1 = async () => {
+  console.log("running keepAlive");
+  const ping = await axios
+    .get("https://disc-bot-t2.onrender.com/")
+    .catch((error) => {
+      console.log(error);
+    });
+  console.log(ping.data);
+};
+
+setInterval(keepAlive1, 13 * 60 * 1000);
+setInterval(keepAlive2, 13 * 60 * 1000);
