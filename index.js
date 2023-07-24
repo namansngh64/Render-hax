9a4901f0e0085b8b9a9a1aa3c888d573d6c5fc15
FalC0n13 <89564649+FalC0n13@users.noreply.github.com>
2023-07-24 22:31:55 +0530

    replit ping

--- index.js
+++ index.js
@@ -46,5 +46,16 @@ const keepAlive1 = async () => {
   console.log(ping.data);
 };
 
+const keepAlive3 = async () => {
+  console.log("running keepAlive");
+  const ping = await axios
+    .get("https://lavalink2.falc0n13.repl.co/")
+    .catch((error) => {
+      console.log(error);
+    });
+  console.log(ping.data);
+};
+
 setInterval(keepAlive2, 12 * 60 * 1000);
 setInterval(keepAlive1, 12 * 60 * 1000);
+setInterval(keepAlive3, 12 * 60 * 1000);
