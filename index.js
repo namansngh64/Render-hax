ef8552dff7510741850f79ffb0742ab6077b32d8
FalC0n13 <89564649+FalC0n13@users.noreply.github.com>
2023-08-18 21:02:51 +0530

    Replit_bot

--- index.js
+++ index.js
@@ -56,6 +56,17 @@ const keepAlive3 = async () => {
   console.log(ping.data);
 };
 
+const keepAlive4 = async () => {
+  console.log("running keepAlive for replit_bot");
+  const ping = await axios
+    .get("https://discbot2.falc0n13.repl.co/")
+    .catch((error) => {
+      console.log(error);
+    });
+  console.log(ping.data);
+};
+
 setInterval(keepAlive2, 12 * 60 * 1000);
+setInterval(keepAlive4, 12 * 60 * 1000);
 setInterval(keepAlive1, 12 * 60 * 1000);
 setInterval(keepAlive3, 12 * 60 * 1000);
